0d31b53ec9c3d31d260d57154100f97517dd1b9f
madx1102 <madx1102@gmail.com>
2024-04-24 11:37:08 +0900

    third

--- midterm/src/Midterm.jsx
+++ midterm/src/Midterm.jsx
@@ -9,9 +9,9 @@ export default function Midterm(){
     const b = () => setF((F)=> F = '바나나');
     return<div>
     <h1>{`당신은 어떤 과일을 좋아하나요?${F}`}</h1>
-    <button id="a" onClick={a}>사과</button>
-    <button id="o"onClick={o}>오렌지</button>
-    <button id="b"onClick={b}>바나나</button>
+    <button onClick={a}>사과</button>
+    <button onClick={o}>오렌지</button>
+    <button onClick={b}>바나나</button>
 </div>;
 }
 
