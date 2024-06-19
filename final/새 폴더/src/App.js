1d19689432e21f8528715290555983464e51782f
madx1102 <madx1102@gmail.com>
2024-06-19 14:49:41 +0900

    tt

---
+++ final/새 폴더/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import BackgroundColorChange from './Final';
+
+function App() {
+  return (
+    <div className="App">
+      <BackgroundColorChange/>
+    </div>
+  );
+}
+
+export default App;
